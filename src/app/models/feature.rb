6e733342531b8ad9417a1b06d0f0008ca700c504
Jose Lloret <j.m.lloret-perez@rgu.ac.uk>
2017-03-14 00:24:15 +0000

    Feature model with enum attribute, validations and default scope for eager loading. #5

--- src/app/models/feature.rb
+++ src/app/models/feature.rb
@@ -1,7 +1,18 @@
 class Feature < ApplicationRecord
-	# Create validation rule for feature_type must be {number, text or options }
+	# Relations
 	belongs_to :product
 	belongs_to :feature_label
 	has_many :feature_options
 	has_many :order_item_features
+
+	# Enum attributes
+	enum feature_type: [:number, :options, :text]
+
+	# Validations
+	validates :feature_type, 
+		inclusion: { in: self.feature_types.keys() ,
+					 message: '%{value} is not a valid feature_type.' }
+
+	# Scope to be applied across all queries to the model
+	default_scope { includes(:feature_label, :feature_options)}
 end
