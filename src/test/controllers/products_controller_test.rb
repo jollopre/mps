b2f93c8fb332dcd1a1bf5f154801e41448b20b5b
Jose Lloret <jollopre@gmail.com>
2017-03-24 03:01:23 +0000

    Adding fixtures two pass products_controller_test. #9

--- src/test/controllers/products_controller_test.rb
+++ src/test/controllers/products_controller_test.rb
@@ -1,11 +1,15 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
-  test 'should get success for existent product' do
+  test 'should get success for index action' do
+  	get products_path
+  	assert_response :success
+  end
+  test 'should get success for show action' do
   	get product_path('1')
   	assert_response :success
   end
-  test 'should get not_found for non-existent product' do
+  test 'should get not_found for show action' do
   	get product_path('10000')
   	assert_response :not_found
   end
