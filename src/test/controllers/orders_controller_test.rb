f99332308fc993f61ba8935ff459cffc07ebe578
Jose Lloret <jollopre@gmail.com>
2017-04-05 00:37:23 +0100

    DRI for orders_controller_test and products_controller_test using fixture methods. #11

--- src/test/controllers/orders_controller_test.rb
+++ src/test/controllers/orders_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
+	setup do
+		@customer = customers(:customer1)
+		@order = orders(:order1)
+	end
+	
 	# POST /orders with body { "order": { "customer_id": number }}
 	test 'should get bad request for missing parameters at create action' do
 		post orders_path, params: {order: {}}
@@ -11,7 +16,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
 		assert_response :bad_request
 	end
 	test 'should get no content for valid data at create action' do
-		post orders_path, params: {order: {customer_id: 1}}
+		post orders_path, params: {order: {customer_id: @customer.id}}
 		assert_response :no_content
 	end
 
@@ -27,7 +32,7 @@ class OrdersControllerTest < ActionDispatch::IntegrationTest
   		assert_response :not_found
 	end
 	test 'should get success at show action' do
-		get order_path(1)
+		get order_path(@order.id)
   		assert_response :ok
 	end
 
--- src/test/controllers/products_controller_test.rb
+++ src/test/controllers/products_controller_test.rb
@@ -1,12 +1,15 @@
 require 'test_helper'
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @product = products(:product1)
+  end
   test 'should get success for index action' do
   	get products_path
   	assert_response :ok
   end
   test 'should get success for show action' do
-  	get product_path('1')
+  	get product_path(@product.id)
   	assert_response :ok
   end
   test 'should get not_found for show action' do
