0b4787e0572aeaf032b5d2a46bebb5578748dd21
Jose Lloret <jollopre@gmail.com>
2017-06-26 14:25:53 +0100

    removed unnecessary gems for running rails API. #19

--- src/app/controllers/application_controller.rb
+++ src/app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::API
+	include ActionController::Helpers
 	include ActionController::HttpAuthentication::Token::ControllerMethods
 	before_action :require_login
 	helper_method :current_user
